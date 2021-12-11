ae80f689a81b9979a67fc707132b3a328763f360
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-12-11 23:02:27 +0900

    商品一覧ページ

--- wine_comparsion_app/routes/web.php
+++ wine_comparsion_app/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/top',function(){
+    return view('items_list');
+})->name('items_list.index');
