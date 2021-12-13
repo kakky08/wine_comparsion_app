0cfd8e50aba3c44d8b24178620ddcad8b3326b9a
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-12-13 17:38:18 +0900

    items_list_pageを更新

--- wine_comparsion_app/resources/views/items_list.blade.php
+++ wine_comparsion_app/resources/views/items_list.blade.php
@@ -24,16 +24,68 @@
     <div class="row h-100">
         {{-- サイドバー --}}
         <div class="col-md-3 pt-5" style="background-color:white;">
-            <form action="" class="">
-                <input type="text" class="space form-control" placeholder="search">
-                <div class="dropdown-menu">
-                    <div class="dropdown-menu"></div>
-                    <button type="button ml-30" class="btn btn-outline-primary">条件を絞り込む</button>
+            <form class="row mb-4">
+                <h3>ワインの名前で検索する</h3>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" placeholder="ワインの名前">
                 </div>
+                    <button type="submit" class="btn btn-primary col-md-2">検索</button>
             </form>
+            {{-- モーダルウィンドウbutton --}}
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                もっと詳しく検索する
+            </button>
+
+            <!-- Modal ここから -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <h3>ワインの種類</h3>
+                                <div class="row">
+                                    <div class="form-check col">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            赤ワイン
+                                        </label>
+                                    </div>
+                                    <div class="form-check col">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            白ワイン
+                                        </label>
+                                    </div>
+                                    <div class="form-check col">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            ロゼ
+                                        </label>
+                                    </div>
+                                    <div class="form-check col">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            スパークリング
+                                        </label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal ここまで -->
         </div>
-        {{-- サイドバー --}}
-        {{-- 右のカラム --}}
+            {{-- サイドバー --}}
+            {{-- 右のカラム --}}
         <div class="col-md-9 px-4 pt-5" style="background-color:rgb(219, 219, 219);">
             <h3 class="border-bottom border-danger border-2 mb-4">あなたにおすすめのワイン</h3>
             {{-- おすすめ一覧ここから --}}
