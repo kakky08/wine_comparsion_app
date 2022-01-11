9d1e8267e6258c5394a5faf32d60420ad35d21fe
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-01-11 23:08:30 +0900

    名前検索機能の実装

--- wine_comparsion_app/resources/views/layouts/items_list_base.blade.php
+++ wine_comparsion_app/resources/views/layouts/items_list_base.blade.php
@@ -24,17 +24,79 @@
     <div class="row h-100">
         {{-- サイドバー --}}
         <div class="col-md-3 pt-5" style="background-color:white;">
-            <form class="row mb-4">
+            <form method="POST" action="{{ route('search.keyword')}}" class="row mb-4">
+                @csrf
                 <h3>ワインの名前で検索する</h3>
                 <div class="col-md-10">
-                    <input type="text" class="form-control" placeholder="ワインの名前">
+                    <input type="text" name="keyword" class="form-control" placeholder="ワインの名前">
                 </div>
                     <button type="submit" class="btn btn-primary col-md-2">検索</button>
             </form>
-            {{-- モーダルウィンドウbutton --}}
+            {{-- Modal Button  --}}
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 もっと詳しく検索する
             </button>
+
+            {{-- Modal start --}}
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Launch static backdrop modal
+            </button>
+            <div class="modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        {{-- form start --}}
+                        <form method="GET" action="{{ route('search.detail')}}" id="search" class="row g-3">
+                            {{-- type select --}}
+                            <div class="col-12">
+                                <label for="inputState" class="form-label">ワインのタイプを選択</label>
+                                <select name="type" id="inputState" class="form-select">
+                                    @foreach ($types as $type)
+                                    <option value="{{ $type->id }}">{{ $type->type }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            {{-- country select --}}
+                            <div class="col-12">
+                                <label for="inputState" class="form-label">生産国を選択</label>
+                                <select name="country" id="inputState" class="form-select">
+                                    @foreach ($countries as $country)
+                                        <option value="{{ $country->id }}">{{ $country->country}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </form>
+                        {{-- form end --}}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+                        <button type="submit" class="btn btn-primary" form="search">検索</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {{-- Modal end --}}
+            {{-- Accordion start --}}
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        ワインの種類で選ぶ
+                    </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                        <ul>
+                            @foreach ($types as $type)
+                                <a href="{{ route('select.by.type', ['id' => $type->id]) }}"><li>{{ $type->type}}</li></a>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            {{-- Accordion end --}}
             <form method="GET" action="{{ route('search.type')}}">
                 <div class="input-group">
                     <label for="exampleFormControlInput1" class="form-label">ワインのタイプを選択</label>
